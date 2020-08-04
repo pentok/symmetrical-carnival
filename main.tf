74970faceae932db075d7d9c7a34e00d5360a8f9
pentok <69121153+pentok@users.noreply.github.com>
2020-08-04 14:57:32 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+﻿variable env.AWS_ACCESS_KEY {
+type = string
+}
+variable env.AWS_REGION {
+type = string
+}
+variable env.AWS_SECRET_KEY {
+type = string
+}
+
+provider "aws" {
+  region     = env.AWS_REGION
+  access_key = env.AWS_ACCESS_KEY
+  secret_key = env.AWS_SECRET_KEY
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+    name     = "testtable"
+    read_capacity = 5
+    write_capacity = 5
+    hash_key = "ID"
+
+    attribute {
+      name = "ID"
+      type = "S"
+    }
+
+
+  
+}
