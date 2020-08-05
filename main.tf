e8af037029a9bee62bfdda9dc1113c2f4d9ab30c
pentok <69121153+pentok@users.noreply.github.com>
2020-08-05 17:42:49 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,20 @@
+﻿provider "aws" {
+  access_key = AWS_ACCESS_KEY
+  region = "us-east-2"
+  secret_key = AWS_SECRET_KEY
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+    name     = "testtable"
+    read_capacity = 5
+    write_capacity = 5
+    hash_key = "ID"
+
+    attribute {
+      name = "ID"
+      type = "S"
+    }
+
+
+  
+}
